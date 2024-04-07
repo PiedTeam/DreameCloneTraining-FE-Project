e73cb8a02a25fd58f95b73de03f49ce4981cff35
jarvietnguyen318@gmail.com <jarvietnguyen318@gmail.com>
2024-04-07 23:50:35 +0700

    feat: initial stucture

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { DatePicker } from 'antd'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <DatePicker />
+    </div>
+  )
+}
