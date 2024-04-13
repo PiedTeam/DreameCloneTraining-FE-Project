02ab4ac38f9a32e0510b5746ab260b8ec3c91ace
Nguyễn Nam Hoàng <nnh53.work@gmail.com>
2024-04-13 14:30:51 +0700

    refactor: fix project config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
+// this file config how vite will bundle react app
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
