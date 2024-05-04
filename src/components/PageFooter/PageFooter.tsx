481706946e22c643023deca31323826430f6dd98
Nguyễn Nam Hoàng <nnh53.work@gmail.com>
2024-05-04 11:56:30 +0700

    Revert "feat: create upload page (#37)"
    This reverts commit 6e2310a710ca3c17c0550ca9f08f347be7db255e.

--- src/components/PageFooter/PageFooter.tsx
+++
@@ -1,6 +0,0 @@
-import { Footer } from 'antd/lib/layout/layout.js';
-
-const PageFooter = (): JSX.Element => {
-  return <Footer style={{ textAlign: 'center' }}>Dreame ©{new Date().getFullYear()} Created by Ant UED</Footer>;
-};
-export default PageFooter;
